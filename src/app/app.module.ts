3e80f22b6085bf3ea52f7233d2827a680d6bea2b
170970 <170970@utags.edu.mx>
2019-09-24 13:30:07 -0500

    Estos son mis cambios recientes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { CarruselComponent } from './components/login/carrusel/carrusel.component';
 import { FormularioComponent } from './components/login/formulario/formulario.component';
+import { NavbarComponent } from './components/menu/navbar/navbar.component';
+import { CardsComponent } from './components/menu/cards/cards.component';
+import { FooterComponent } from './components/menu/footer/footer.component';
 
 
 @NgModule({
@@ -15,7 +18,10 @@ import { FormularioComponent } from './components/login/formulario/formulario.co
     AppComponent,
     LoginComponent,
     CarruselComponent,
-    FormularioComponent
+    FormularioComponent,
+    NavbarComponent,
+    CardsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
