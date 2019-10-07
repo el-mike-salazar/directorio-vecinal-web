89ed1b2bc4bc7a4f7b4916fd7e786d2090760def
Jonathan  Ortega Villanueva <170970@utags.edu.mx>
2019-10-07 14:02:29 -0500

    Actualizacion de archivos DVW-34

--- src/app/components/login/formulario/formulario.component.ts
+++ src/app/components/login/formulario/formulario.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {  Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-form',
@@ -8,20 +8,10 @@ import {  Router } from '@angular/router';
 })
 export class FormularioComponent implements OnInit {
 
-  constructor( private _route: Router ) { }
+ 
 
   ngOnInit() {
-    // Swal.fire({
-    //   title: 'Error!',
-    //   text: 'hola pakito',
-    //   type: 'error',
-    //   confirmButtonText: 'Cool'
-    // });
-  }
 
-  toMenu() {
-    console.log('boton funcionando');
-    this._route.navigate(['/menu']);
   }
 
 }
