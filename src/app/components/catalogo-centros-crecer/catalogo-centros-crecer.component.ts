3a46c755503054daa4f19b53fbe55abeb11688b4
José Francisco  Tapia Macías <francisco.tapia@utags.edu.mx>
2019-10-17 13:32:46 -0500

    Estructuracion de catalogo centros crecer

---
+++ src/app/components/catalogo-centros-crecer/catalogo-centros-crecer.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { CCrecerModel } from '.././../../models/cCrecer.model';
+
+@Component({
+  selector: 'app-catalogo-centros-crecer',
+  templateUrl: './catalogo-centros-crecer.component.html',
+  styleUrls: ['./catalogo-centros-crecer.component.css']
+})
+export class CatalogoCentrosCrecerComponent {
+
+  centro: CCrecerModel;
+  cCrecer: CCrecerModel[] = [];
+
+  paquetito = {
+    editarCentroCrecerComponent: false,
+    tablaCentrosCrecerComponent: true,
+    data: {}
+  };
+
+
+  constructor() {
+  }
+
+  actCCrecer(cCrecer){
+    this.cCrecer = cCrecer;
+  }
+
+}
